26a062d4c7d281366197985255bb96a32ed27e9c
MacInnes <califormula@gmail.com>
2018-11-06 12:34:38 -0700

    removes pry

--- app/serializers/driver_serializer.rb
+++ app/serializers/driver_serializer.rb
@@ -3,7 +3,6 @@ class DriverSerializer
   attributes :id, :username, :email, :destination
 
   attribute :formatted_destination do |object|
-    binding.pry
     if object.destination
       uri = object.destination.gsub(/ /, '+')
       "https://www.google.com/maps/embed/v1/place?key=#{ENV['GOOGLE_MAPS_API_KEY']}&q=#{uri}"
