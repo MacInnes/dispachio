e1af299c5cac5442ad3d8d280f7b28555f11d8cf
MacInnes <califormula@gmail.com>
2018-11-06 13:22:32 -0700

    creates ajax call to api/drivers/:id/destination that updates the map every 10 seconds

--- app/assets/javascripts/driver.js
+++ app/assets/javascripts/driver.js
@@ -1,3 +1,21 @@
-$(document).ready(function(){
-  console.log("I'm a driver!")
-})
+function loop() {
+  console.log(api_key);
+  console.log(driver_id);
+  getDestination();
+  setInterval(loop, 10000);
+}
+
+function getDestination(){
+  $.ajax({
+    dataType: "json",
+    url: '/api/v1/drivers/' + driver_id + '/destination',
+    headers: {
+      'X-API-KEY': api_key
+    },
+    success: function(data){
+      $('#driver-iframe').attr('src', data.data.attributes.formatted_destination)
+    }
+  });
+}
+
+loop();
