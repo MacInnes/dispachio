e0a610407c279c61ba2fc30e608d2739957137e6
MacInnes <califormula@gmail.com>
2018-11-05 21:30:15 -0700

    adds driver validation to api/v1/driver_controller'

--- app/controllers/api/v1/driver_controller.rb
+++ app/controllers/api/v1/driver_controller.rb
@@ -1,8 +1,18 @@
 class Api::V1::DriverController < ActionController::API
 
   def show
-    user = User.find(params[:id])
-    render json: DriverSerializer.new(user).serialized_json
+    if valid_user?
+      render json: DriverSerializer.new(@user).serialized_json
+    else
+      render status: 403, json: {message: "Unauthorized"}
+    end
+  end
+
+  private
+
+  def valid_user?
+    @user ||= User.find(params[:id])
+    @user.api_key == request.headers['HTTP_X_API_KEY']
   end
 
 end
--- spec/requests/api/v1/requests/driver_destination_spec.rb
+++ spec/requests/api/v1/requests/driver_destination_spec.rb
@@ -3,8 +3,11 @@ require 'rails_helper'
 describe 'Driver' do
   it 'can get their destination' do
     driver = create(:user, role: 'driver', destination: '1331 17th St, Denver, CO, 80202')
+    driver.generate_api_key
 
-    get "/api/v1/drivers/#{driver.id}/destination"
+    headers = { "CONTENT_TYPE" => "application/json", "X-API-KEY" => driver.api_key }
+
+    get "/api/v1/drivers/#{driver.id}/destination", headers: headers
 
     body = JSON.parse(response.body, symbolize_names: true)
 
