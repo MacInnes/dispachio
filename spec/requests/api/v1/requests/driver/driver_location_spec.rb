6b6f16105a6f5b7392697e666a6939c71b5f61b3
MacInnes <califormula@gmail.com>
2018-11-26 18:16:53 -0700

    adds test to ensure a driver can't update another driver's location

--- spec/requests/api/v1/requests/driver/driver_location_spec.rb
+++ spec/requests/api/v1/requests/driver/driver_location_spec.rb
@@ -20,8 +20,35 @@ describe 'Driver' do
 
     expect(response.status).to eq(204)
 
-    updated_driver = User.first
+    updated_driver = User.find(driver.id)
     expect(updated_driver.lat).to eq('39.7507834')
     expect(updated_driver.long).to eq('-104.9964355')
+  end
+
+  it "can't update another driver's location" do
+    driver = create(:user)
+    driver.generate_api_key
+
+    driver_2 = create(:user)
+    driver_2.generate_api_key
+
+    headers = {
+      "CONTENT_TYPE": "application/json",
+      'X-API-KEY': driver.api_key
+    }
+
+    payload = {
+      lat: '39.7507834',
+      long: '-104.9964355'
+    }
+
+    patch "/api/v1/drivers/#{driver_2.id}/location", headers: headers, params: payload.to_json
+
+
+    expect(response.status).to eq(403)
+
+    updated_driver_2 = User.find(driver_2.id)
+    expect(updated_driver_2.lat).to eq('123.123')
+    expect(updated_driver_2.long).to eq('-123.123')
   end
 end
